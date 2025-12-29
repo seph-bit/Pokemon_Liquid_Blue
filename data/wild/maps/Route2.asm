293ac12281fb0e9698f9dc6193b3909391b1ff98
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 12:46:37 -0600

    Update Route2.asm

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -1,23 +1,23 @@
 Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, RATTATA
-	db  3, PIDGEY
+	db  5, RATTATA
+	db  6, PIDGEY
 	db  4, PIDGEY
 	db  4, RATTATA
-	db  5, PIDGEY
+	db  5, ODDISH
 IF DEF(_RED)
 	db  3, WEEDLE
-	db  2, RATTATA
-	db  5, RATTATA
 	db  4, WEEDLE
-	db  5, WEEDLE
+	db  6, ODDISH
+	db  4, CATERPIE
+	db  5, CATERPIE
 ENDC
 IF DEF(_BLUE)
 	db  3, CATERPIE
-	db  2, RATTATA
-	db  5, RATTATA
 	db  4, CATERPIE
-	db  5, CATERPIE
+	db  6, ODDISH
+	db  4, WEEDLE
+	db  5, WEEDLE
 ENDC
 	end_grass_wildmons
 
