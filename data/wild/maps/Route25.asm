c4cfda358f177c6e8709f398c297826e5beca160
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 13:17:30 -0600

    Update Route25.asm

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,28 +1,28 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db  8, WEEDLE
-	db  9, KAKUNA
 	db 13, PIDGEY
-	db 12, ODDISH
+	db 15, KRABBY
+	db 14, PIDGEY
 	db 13, ODDISH
-	db 12, ABRA
-	db 14, ODDISH
-	db 10, ABRA
-	db  7, METAPOD
-	db  8, CATERPIE
+	db 15, ODDISH
+	db 14, ABRA
+	db 16, PIDGEOTTO
+	db 15, ABRA
+	db 13, BELLSPROUT
+	db 16, BELLSPROUT
 ENDC
 IF DEF(_BLUE)
-	db  8, CATERPIE
-	db  9, METAPOD
 	db 13, PIDGEY
-	db 12, BELLSPROUT
+	db 15, KRABBY
+	db 14, PIDGEY
 	db 13, BELLSPROUT
-	db 12, ABRA
-	db 14, BELLSPROUT
-	db 10, ABRA
-	db  7, KAKUNA
-	db  8, WEEDLE
+	db 15, BELLSPROUT
+	db 14, ABRA
+	db 16, PIDGEOTTO
+	db 15, ABRA
+	db 13, ODDISH
+	db 16, ODDISH
 ENDC
 	end_grass_wildmons
 
