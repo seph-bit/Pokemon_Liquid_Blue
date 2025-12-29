241c41e264b9d57fb1c83ce8d1053584fb79761f
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 13:47:09 -0600

    Update Route10.asm

--- data/wild/maps/Route10.asm
+++ data/wild/maps/Route10.asm
@@ -1,25 +1,25 @@
 Route10WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, VOLTORB
-	db 16, SPEAROW
-	db 14, VOLTORB
+	db 20, MAGNEMITE
+	db 21, FEAROW
+	db 19, VOLTORB
 IF DEF(_RED)
-	db 11, EKANS
-	db 13, SPEAROW
-	db 15, EKANS
-	db 17, VOLTORB
-	db 17, SPEAROW
-	db 13, EKANS
-	db 17, EKANS
+	db 22, PSYDUCK
+	db 22, FEAROW
+	db 24, ARBOK
+	db 23, MAGNEMITE
+	db 25, MACHOP
+	db 24, SANDSLASH
+	db 26, SANDSLASH
 ENDC
 IF DEF(_BLUE)
-	db 11, SANDSHREW
-	db 13, SPEAROW
-	db 15, SANDSHREW
-	db 17, VOLTORB
-	db 17, SPEAROW
-	db 13, SANDSHREW
-	db 17, SANDSHREW
+	db 22, SLOWPOKE
+	db 22, SPEAROW
+	db 24, SANDSLASH
+	db 23, MAGNEMITE
+	db 25, MACHOP
+	db 24, ARBOK
+	db 26, ARBOK
 ENDC
 	end_grass_wildmons
 
