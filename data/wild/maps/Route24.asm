856d7b381bb5084ed3ff460441f88911ee96fca8
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 13:13:02 -0600

    Update Route24.asm

--- data/wild/maps/Route24.asm
+++ data/wild/maps/Route24.asm
@@ -1,26 +1,26 @@
 Route24WildMons:
 	def_grass_wildmons 25 ; encounter rate
 IF DEF(_RED)
-	db  7, WEEDLE
-	db  8, KAKUNA
-	db 12, PIDGEY
 	db 12, ODDISH
-	db 13, ODDISH
-	db 10, ABRA
+	db 13, VENONAT
+	db 14, PIDGEY
 	db 14, ODDISH
+	db 15, PIDGEY
+	db 13, ABRA
+	db 16, BELLSPROUT
 ENDC
 IF DEF(_BLUE)
-	db  7, CATERPIE
-	db  8, METAPOD
-	db 12, PIDGEY
-	db 12, BELLSPROUT
-	db 13, BELLSPROUT
-	db 10, ABRA
+	db 12, ODDISH
+	db 13, VENONAT
+	db 14, PIDGEY
 	db 14, BELLSPROUT
+	db 15, PIDGEY
+	db 13, ABRA
+	db 16, ODDISH
 ENDC
-	db 13, PIDGEY
-	db  8, ABRA
-	db 12, ABRA
+	db 16, PIDGEOTTO
+	db 17, ABRA
+	db 16, ABRA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
