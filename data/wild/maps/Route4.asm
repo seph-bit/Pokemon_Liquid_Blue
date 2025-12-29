3a52581fae3138bbc5ff4d4351bada205a7b8af6
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 13:07:49 -0600

    Update Route4.asm

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -1,25 +1,25 @@
 Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 10, RATTATA
-	db 10, SPEAROW
-	db  8, RATTATA
+	db 13, MANKEY
+	db 13, SPEAROW
+	db 15, MANKEY
 IF DEF(_RED)
-	db  6, EKANS
-	db  8, SPEAROW
-	db 10, EKANS
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, EKANS
 	db 12, EKANS
+	db 14, SPEAROW
+	db 14, EKANS
+	db 12, PSYDUCK
+	db 15, PSYDUCK
+	db 14, SANDSHREW
+	db 12, SANDSHREW
 ENDC
 IF DEF(_BLUE)
-	db  6, SANDSHREW
-	db  8, SPEAROW
-	db 10, SANDSHREW
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, SANDSHREW
 	db 12, SANDSHREW
+	db 14, SPEAROW
+	db 14, SANDSHREW
+	db 12, SLOWPOKE
+	db 15, SLOWPOKE
+	db 12, EKANS
+	db 14, EKANS
 ENDC
 	end_grass_wildmons
 
