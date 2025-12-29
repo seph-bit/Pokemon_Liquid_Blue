1ac14cfe5b19f98a77f9b1025a3cefff4f559058
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 12:39:17 -0600

    Update Route22.asm

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,27 +1,27 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, RATTATA
+	db  4, MANKEY
 IF DEF(_RED)
 	db  3, NIDORAN_M
-	db  4, RATTATA
+	db  4, POLIWAG
 	db  4, NIDORAN_M
-	db  2, RATTATA
-	db  2, NIDORAN_M
-	db  3, SPEAROW
-	db  5, SPEAROW
-	db  3, NIDORAN_F
+	db  5, MANKEY
+	db  5, NIDORAN_M
+	db  6, POLIWAG
+	db  5, POLIWAG
 	db  4, NIDORAN_F
+	db  6, NIDORAN_F
 ENDC
 IF DEF(_BLUE)
 	db  3, NIDORAN_F
-	db  4, RATTATA
+	db  4, POLIWAG
 	db  4, NIDORAN_F
-	db  2, RATTATA
-	db  2, NIDORAN_F
-	db  3, SPEAROW
-	db  5, SPEAROW
-	db  3, NIDORAN_M
+	db  5, MANKEY
+	db  5, NIDORAN_F
+	db  6, POLIWAG
+	db  5, POLIWAG
 	db  4, NIDORAN_M
+	db  6, NIDORAN_M
 ENDC
 	end_grass_wildmons
 
