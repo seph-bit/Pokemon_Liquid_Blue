cd23b0dcf634856f716d08b2709a7f6367e8c1ec
seph-bit <seraphim.beatle@gmail.com>
2025-12-30 11:24:07 -0600

    Update PokemonMansion3F.asm

--- data/wild/maps/PokemonMansion3F.asm
+++ data/wild/maps/PokemonMansion3F.asm
@@ -1,28 +1,28 @@
 PokemonMansion3FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 31, KOFFING
-	db 33, GROWLITHE
-	db 35, KOFFING
-	db 32, PONYTA
-	db 34, PONYTA
-	db 40, WEEZING
-	db 34, GRIMER
-	db 38, WEEZING
-	db 36, PONYTA
-	db 42, MUK
+	db 37, KOFFING
+	db 39, GROWLITHE
+	db 39, KOFFING
+	db 38, PONYTA
+	db 40, GRIMER
+	db 44, WEEZING
+	db 40, MAGMAR
+	db 43, WEEZING
+	db 45, RAPIDASH
+	db 46, MUK
 ENDC
 IF DEF(_BLUE)
-	db 31, GRIMER
-	db 33, VULPIX
-	db 35, GRIMER
-	db 32, PONYTA
-	db 34, MAGMAR
-	db 40, MUK
-	db 34, KOFFING
-	db 38, MUK
-	db 36, PONYTA
-	db 42, WEEZING
+	db 37, GRIMER
+	db 39, VULPIX
+	db 39, GRIMER
+	db 38, PONYTA
+	db 40, MAGMAR
+	db 44, MUK
+	db 40, KOFFING
+	db 43, MAGMAR
+	db 45, RAPIDASH
+	db 46, WEEZING
 ENDC
 	end_grass_wildmons
 
