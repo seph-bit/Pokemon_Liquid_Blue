d17a4ea7c354fd5ffa5603097209f5f069afa1ff
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 19:19:43 -0600

    Update PowerPlant.asm

--- data/wild/maps/PowerPlant.asm
+++ data/wild/maps/PowerPlant.asm
@@ -1,20 +1,19 @@
-PowerPlantWildMons:
-	def_grass_wildmons 10 ; encounter rate
+IFdmons 10 ; encounter rate
 	db 21, VOLTORB
 	db 21, MAGNEMITE
 	db 20, PIKACHU
-	db 24, PIKACHU
-	db 23, MAGNEMITE
-	db 23, VOLTORB
+	db 24, PORYGON
+	db 23, RAICHU
+	db 23, ELECTRODE
 	db 32, MAGNETON
 	db 35, MAGNETON
 IF DEF(_RED)
-	db 33, ELECTABUZZ
+	db 38, ELECTABUZZ
 	db 36, ELECTABUZZ
 ENDC
 IF DEF(_BLUE)
-	db 33, RAICHU
-	db 36, RAICHU
+	db 38, RAICHU
+	db 40, RAICHU
 ENDC
 	end_grass_wildmons
 
