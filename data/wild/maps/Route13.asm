5ba0fc20bcabcd1d33b472fba5ec4d50f0690b57
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 18:52:47 -0600

    Update Route13.asm

--- data/wild/maps/Route13.asm
+++ data/wild/maps/Route13.asm
@@ -1,28 +1,28 @@
 Route13WildMons:
 	def_grass_wildmons 20 ; encounter rate
 IF DEF(_RED)
-	db 24, ODDISH
-	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
-	db 25, DITTO
-	db 28, GLOOM
 	db 30, GLOOM
+	db 31, PIDGEOTTO
+	db 29, ODDISH
+	db 24, VENONAT
+	db 32, SHELLDER
+	db 33, PIDGEOTTO
+	db 35, STARYU
+	db 31, DITTO
+	db 34, WEEPINBELL
+	db 36, WEEPINBELL
 ENDC
 IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 25, DITTO
-	db 28, WEEPINBELL
 	db 30, WEEPINBELL
+	db 31, PIDGOTTO
+	db 29, BELLSPROUT
+	db 24, VENONAT
+	db 32, STARYU
+	db 33, PIDGEOTTO
+	db 35, SHELLDER
+	db 31, DITTO
+	db 34, GLOOM
+	db 36, GLOOM
 ENDC
 	end_grass_wildmons
 
