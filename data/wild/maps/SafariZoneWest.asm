d1d9902489d89a6826fd58ac04f9be3b7d929989
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 23:16:06 -0600

    Update SafariZoneWest.asm

--- data/wild/maps/SafariZoneWest.asm
+++ data/wild/maps/SafariZoneWest.asm
@@ -1,26 +1,26 @@
 SafariZoneWestWildMons:
 	def_grass_wildmons 30 ; encounter rate
 IF DEF(_RED)
-	db 25, NIDORAN_M
-	db 26, DODUO
-	db 23, VENONAT
-	db 24, EXEGGCUTE
-	db 33, NIDORINO
-	db 26, EXEGGCUTE
-	db 25, NIDORAN_F
+	db 36, EXEGGCUTE
+	db 34, GLOOM
+	db 32, BEEDRILL
+	db 35, DODRIO
+	db 33, PIKACHU
+	db 38, ARCANINE
+	db 37, GENGAR
 ENDC
 IF DEF(_BLUE)
-	db 25, NIDORAN_F
-	db 26, DODUO
-	db 23, VENONAT
-	db 24, EXEGGCUTE
-	db 33, NIDORINA
-	db 26, EXEGGCUTE
-	db 25, NIDORAN_M
+	db 36, EXEGGCUTE
+	db 34, WEEPINBELL
+	db 32, BUTTERFREE
+	db 35, DODRIO
+	db 33, EEVEE
+	db 38, NINETALES
+	db 37, GENGAR
 ENDC
-	db 31, VENOMOTH
-	db 26, TAUROS
-	db 28, KANGASKHAN
+	db 36, TAUROS
+	db 39, TAUROS
+	db 38, KANGASKHAN
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
