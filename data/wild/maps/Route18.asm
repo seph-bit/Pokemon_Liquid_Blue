abab54092c1fdf3626ded894fd4fa35b9b30a035
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 19:15:39 -0600

    Update Route18.asm

--- data/wild/maps/Route18.asm
+++ data/wild/maps/Route18.asm
@@ -1,15 +1,15 @@
 Route18WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 25, RATICATE
-	db 24, DODUO
-	db 25, FEAROW
-	db 26, DODUO
-	db 28, DODUO
-	db 29, RATICATE
-	db 27, FEAROW
-	db 29, FEAROW
+	db 31, PONYTA
+	db 30, FEAROW
+	db 35, RATICATE
+	db 34, EXEGGCUTE
+	db 33, FEAROW
+	db 32, DODUO
+	db 36, DODRIO
+	db 37, RATICATE
+	db 35, EXEGGCUTE
+	db 38, EXEGGCUTE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
