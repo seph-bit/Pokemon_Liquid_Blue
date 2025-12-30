41e980fe1d80c3862a04f401ff4f313ca8b265c9
seph-bit <seraphim.beatle@gmail.com>
2025-12-30 00:21:01 -0600

    Update SeafoamIslandsB3F.asm

--- data/wild/maps/SeafoamIslandsB3F.asm
+++ data/wild/maps/SeafoamIslandsB3F.asm
@@ -1,28 +1,28 @@
 SeafoamIslandsB3FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 31, SLOWPOKE
-	db 31, SEEL
-	db 33, SLOWPOKE
-	db 33, SEEL
-	db 29, HORSEA
-	db 31, SHELLDER
-	db 31, HORSEA
-	db 29, SHELLDER
-	db 39, SEADRA
+	db 34, SHELLDER
+	db 35, SEEL
+	db 37, HORSEA
+	db 36, SEEL
+	db 38, KRABBY
+	db 39, SHELLDER
+	db 40, STARYU
+	db 42, CLOYSTER
+	db 41, SEADRA
 ENDC
 IF DEF(_BLUE)
-	db 31, PSYDUCK
-	db 31, SEEL
-	db 33, PSYDUCK
-	db 33, SEEL
-	db 29, KRABBY
-	db 31, STARYU
-	db 31, KRABBY
-	db 29, STARYU
-	db 39, KINGLER
+	db 34, STARYU
+	db 35, SEEL
+	db 37, KRABBG
+	db 36, SEEL
+	db 38, HORSEA
+	db 39, STARYU
+	db 40, SHELLDER
+	db 42, STARMIE
+	db 41, KINGLER
 ENDC
-	db 37, DEWGONG
+	db 43, DEWGONG
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
