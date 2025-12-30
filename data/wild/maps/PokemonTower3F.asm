79715811a5a24936bd5dc5fb755eeed45f7b6232
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 18:19:57 -0600

    Update PokemonTower3F.asm

--- data/wild/maps/PokemonTower3F.asm
+++ data/wild/maps/PokemonTower3F.asm
@@ -4,12 +4,12 @@ PokemonTower3FWildMons:
 	db 21, GASTLY
 	db 22, GASTLY
 	db 23, GASTLY
-	db 19, GASTLY
-	db 18, GASTLY
 	db 24, GASTLY
-	db 20, CUBONE
-	db 22, CUBONE
-	db 25, HAUNTER
+	db 25, GASTLY
+	db 26, GASTLY
+	db 23, CUBONE
+	db 25, CUBONE
+	db 28, HAUNTER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
