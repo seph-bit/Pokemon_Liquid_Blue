792f1f0630a7ae8099f4bdfd02d97953876425d0
seph-bit <seraphim.beatle@gmail.com>
2025-12-29 18:59:46 -0600

    Update Route14.asm

--- data/wild/maps/Route14.asm
+++ data/wild/maps/Route14.asm
@@ -1,27 +1,27 @@
 Route14WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 24, ODDISH
-	db 26, PIDGEY
+	db 24, WEEPINBELL
+	db 26, PIDGEOTTO
 	db 23, DITTO
-	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
+	db 24, VENOMOTH
+	db 22, GLOOM
+	db 26, VENOMOTH
+	db 26, KOFFING
 	db 30, GLOOM
 ENDC
 IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 26, PIDGEY
+	db 24, WEEPINBELL
+	db 26, PIDGEOTTO
 	db 23, DITTO
-	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 30, WEEPINBELL
+	db 24, VENOMOTH
+	db 22, WEEPINBELL
+	db 26, STARYU
+	db 26, GRIMER
+	db 30, GLOOM
 ENDC
-	db 28, PIDGEOTTO
-	db 30, PIDGEOTTO
+	db 28, VENOMOTHT
+    dbdb 37, VENOMOTH
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
