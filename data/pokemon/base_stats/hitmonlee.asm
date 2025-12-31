8eab12b2d198472f78caeb9c46210605ab795a00
seph-bit <seraphim.beatle@gmail.com>
2025-12-31 12:22:08 -0600

    Update hitmonlee.asm

--- data/pokemon/base_stats/hitmonlee.asm
+++ data/pokemon/base_stats/hitmonlee.asm
@@ -1,10 +1,10 @@
 	db DEX_HITMONLEE ; pokedex id
 
-	db  50, 120,  53,  87,  35
+	db  75, 120,  65,  95,  60
 	;   hp  atk  def  spd  spc
 
 	db FIGHTING, FIGHTING ; type
-	db 45 ; catch rate
+	db 75 ; catch rate
 	db 139 ; base exp
 
 	INCBIN "gfx/pokemon/front/hitmonlee.pic", 0, 1 ; sprite dimensions
