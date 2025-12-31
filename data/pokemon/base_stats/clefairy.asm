ca582d883b727565f81520758c8838c64470cb4f
seph-bit <seraphim.beatle@gmail.com>
2025-12-31 12:12:47 -0600

    Update clefairy.asm

--- data/pokemon/base_stats/clefairy.asm
+++ data/pokemon/base_stats/clefairy.asm
@@ -1,10 +1,10 @@
 	db DEX_CLEFAIRY ; pokedex id
 
-	db  70,  45,  48,  35,  60
+	db  70,  45,  48,  35,  70
 	;   hp  atk  def  spd  spc
 
 	db NORMAL, NORMAL ; type
-	db 150 ; catch rate
+	db 190 ; catch rate
 	db 68 ; base exp
 
 	INCBIN "gfx/pokemon/front/clefairy.pic", 0, 1 ; sprite dimensions
