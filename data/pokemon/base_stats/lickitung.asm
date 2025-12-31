bc7f4aeebc7f72cb06a0954c16b87f2afb38b938
seph-bit <seraphim.beatle@gmail.com>
2025-12-31 12:24:58 -0600

    Update lickitung.asm

--- data/pokemon/base_stats/lickitung.asm
+++ data/pokemon/base_stats/lickitung.asm
@@ -1,16 +1,16 @@
 	db DEX_LICKITUNG ; pokedex id
 
-	db  90,  55,  75,  30,  60
+	db  95,  70,  80,  45,  70
 	;   hp  atk  def  spd  spc
 
 	db NORMAL, NORMAL ; type
-	db 45 ; catch rate
+	db 100 ; catch rate
 	db 127 ; base exp
 
 	INCBIN "gfx/pokemon/front/lickitung.pic", 0, 1 ; sprite dimensions
 	dw LickitungPicFront, LickitungPicBack
 
-	db WRAP, SUPERSONIC, NO_MOVE, NO_MOVE ; level 1 learnset
+	db WRAP, SUPERSONIC, LICK, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
