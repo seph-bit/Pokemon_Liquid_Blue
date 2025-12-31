f347c9ccdf6ae07dc021b19b363284cca47b3f05
seph-bit <seraphim.beatle@gmail.com>
2025-12-31 12:43:56 -0600

    Update rapidash.asm

--- data/pokemon/base_stats/rapidash.asm
+++ data/pokemon/base_stats/rapidash.asm
@@ -1,10 +1,10 @@
 	db DEX_RAPIDASH ; pokedex id
 
-	db  65, 100,  70, 105,  80
+	db  65, 100,  70, 115,  85
 	;   hp  atk  def  spd  spc
 
-	db FIRE, FIRE ; type
-	db 60 ; catch rate
+	db FIRE, NORMAL ; type
+	db 85 ; catch rate
 	db 192 ; base exp
 
 	INCBIN "gfx/pokemon/front/rapidash.pic", 0, 1 ; sprite dimensions
